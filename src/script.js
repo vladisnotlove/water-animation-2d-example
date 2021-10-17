d3750659660a3d7b4b7b32bf811b52e6f4903959
Vladislav Nikolaev <vladisnotlove@gmail.com>
2021-10-17 21:30:35 +0300

    Add playing animation by mouse

--- src/script.js
+++ src/script.js
@@ -2,7 +2,36 @@ import {WaterAnimation2d} from 'water-animation-2d';
 
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas");
-  const waterAnimation2d = new WaterAnimation2d(canvas);
+  canvas.width = window.innerWidth;
+  const waterAnim = new WaterAnimation2d(canvas);
 
-  waterAnimation2d.run();
+  waterAnim.run();
+
+  canvas.addEventListener("mousemove", (e) => {
+    const canvasRect = canvas.getBoundingClientRect();
+    const cur = {
+      x: e.clientX - canvasRect.x,
+      y: e.clientY - canvasRect.y,
+    }
+    const prev = {
+      x: cur.x - e.movementX,
+      y: cur.y - e.movementY
+    }
+
+    if (cur.x > 0 && cur.x < canvasRect.right) {
+      if (
+          waterAnim.isUnderSurface(prev.x, prev.y) && !waterAnim.isUnderSurface(cur.x, cur.y) ||
+          !waterAnim.isUnderSurface(prev.x, prev.y) && waterAnim.isUnderSurface(cur.x, cur.y)
+      ) {
+        const id = waterAnim.applyForce(cur.x, {
+          x: e.movementX * 100,
+          y: e.movementY * 100
+        });
+
+        setTimeout(() => {
+          waterAnim.cancelForce(id)
+        }, waterAnim.deltaTime * 1000 * 5)
+      }
+    }
+  })
 })
